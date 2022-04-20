42181e01fa6e74256adee1c8647a7836f7c6a877
Nitin Singh <nitinsinghsaini8@gmail.com>
2022-04-20 18:05:35 +0530

    third commit

--- Bookkeeping/settings.py
+++ Bookkeeping/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yi5+m6s-r*q+fbam0wr@nl!c7gq*mhq+zp20ykaj=&7nbvutsg'
+SECRET_KEY = os.environ['secret_key']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
